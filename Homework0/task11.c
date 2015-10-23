bfe1def910ff66fb714af328b703275ea93b973f
Любовь Степанова <zettom@ya.ru>
2015-10-23 13:33:30 +0300

    Update task11.c

--- Homework0/task11.c
+++ Homework0/task11.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
+int main(){
     int n,i;
     scanf("%d", &n);
     int fib;
     int fib0 = 0;
     int fib1 = 1;
-    for(i = 0; i<(n-1); ++i){
+    for(i=0; i<(n-1); ++i){
         fib = fib1 + fib0;
         fib0 = fib1;
         fib1 = fib;
     }
-    if(n != 0) {
+    if(n!=0) {
             printf("%d", fib1);
     }
     else {
