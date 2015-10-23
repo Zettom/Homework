f59ce24e8282c4cbc1b637f6529d665a4ebcb7fa
Любовь Степанова <zettom@ya.ru>
2015-10-23 13:30:43 +0300

    Update task5.c

--- Homework0/task5.c
+++ Homework0/task5.c
@@ -4,16 +4,16 @@
 int main()
 {
     const n = 5;
-    srand( time( 0 ) );
+    srand(time(0));
     int array[n];
     int i;
     int zeros = 0;
-    for(i = 0; i<n; ++i){
+    for(i=0; i<n; ++i){
         array[i] = rand() % 3;
         printf("%d\n", array[i]);
     }
     for (i=0; i< n/2; ++i){
-        if(array[i] != array[n-i-1]) {
+        if(array[i]!=array[n-i-1]) {
             printf("No");
             return 0;
         }
