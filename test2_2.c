f64a03d02186bcbc24ef44d1c98e8dd511c8681c
Zettom <zettom@ya.ru>
2015-12-25 14:11:00 +0300

    Create test2_2.c

---
+++ test2_2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main ()
+{
+    char num1[100] = "10011";
+    char num2[100] = "0111";
+    int dif;
+    int max = strlen(num1) > strlen(num2) ? strlen(num1) : strlen(num2);
+    int i;
+    for (i = strlen(num1); i < max; i++)
+    {
+        num1[i] = '0';
+    }
+    for (i = strlen(num2); i < max; i++)
+    {
+        num2[i] = '0';
+    }
+    i = max - 1;
+    while ((num1[i] == num2[i]) && (num1[i] != '\0'))
+    {
+        i--;
+    }
+    dif = num1[i] - num2[i];
+    printf("%d", dif);
+}
