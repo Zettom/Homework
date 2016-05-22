14df16cffe3180036f336f1ec7039574007b9c5f
Zettom <zettom@ya.ru>
2016-05-22 23:42:24 +0300

    Create balance.scala

---
+++ Scala/balance.scala
@@ -0,0 +1,18 @@
+object Balance {
+  def main(args: Array[String]) {
+    def balance(chars: List[Char]): Boolean = {
+      def numbs(ls: List[Char], op: Int): Boolean = {
+        if (ls.isEmpty) op == 0
+        else if (ls.head == '(') numbs(ls.tail, op + 1)
+        else if (ls.head == ')'){
+          if (op != 0) numbs(ls.tail, op - 1)
+          else false
+        }
+        else numbs(ls.tail, op)
+      }
+      numbs(chars, 0)
+    }
+
+    balance(")()(()(just an))*()( example".toList)
+  }
+}
